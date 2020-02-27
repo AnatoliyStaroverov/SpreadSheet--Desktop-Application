35093d3a9e798cf038cd97a738db0be9ccb3d1d7
Anatoliy Staroverov <anatoliy.staroverov@wsu.edu>
2020-02-26 21:07:15 -0800

    added DLL Library.

---
+++ SpreedsheetEngine/Class1.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Class1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SpreedsheetEngine
+{
+    /// <summary>
+    /// Class.
+    /// </summary>
+    public class Class1
+    {
+    }
+}
